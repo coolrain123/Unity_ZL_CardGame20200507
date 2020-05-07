42d904bedb8310c2479d05c1e683032a82a29ec9
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-05-07 21:33:43 +0800

    JSON

---
+++ Unity_CardGame/Assets/Scripts/GetCard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;//網路連線API
+
+public class GetCard : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(GetCardData());
+    }
+
+    public IEnumerator GetCardData()
+    {
+        //引用 (網路要求 www = 網路要求.Post("網址",""))
+        using (UnityWebRequest www = UnityWebRequest.Post("https://script.google.com/macros/s/AKfycbxa_hlodN_iqO-I1Cj8DDTB0J68rnxm7v9AXhyRaVTUzkaQC-w/exec", ""))
+        {
+            //等待 網路要求時間
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                print("網路錯誤" + www.error);
+            }
+            else
+            {
+                print(www.downloadHandler.text);
+            }
+        }
+    }
+}
