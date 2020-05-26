1303b7308abbfcaf0e28ce4937c2606d85fc2ebd
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-05-26 15:47:48 +0800

    +-號按鈕

--- Unity_CardGame/Assets/Scripts/DeckManager.cs
+++ Unity_CardGame/Assets/Scripts/DeckManager.cs
@@ -13,55 +13,85 @@ public class DeckManager : MonoBehaviour
     [Header("牌組內容")]
     public Transform contentDeck;
     [Header("卡牌數量")]
-    public GameObject TextCountDeck;
+    public Text TextCountDeck;
+    [Header("開始遊戲按鈕")]
+    public Button btnStart;
 
     private void Awake()
     {
         instance = this;
+
+        btnStart.interactable = false;
     }
 
     public void AddCard(int index)
     {
         if (deck.Count < 30)
         {
+            //選取的卡牌
+            CardData card = GetCard.instance.cards[index - 1];
             //尋找要增加卡牌在清單的資料
             // " => " 黏巴達 (Lambda C#7)
             // 相同卡牌 = 牌組.尋找全部(卡牌 => 卡牌.相同(目前點選的卡牌資訊))
-            List<CardData> sameCard = deck.FindAll(c => c.Equals(GetCard.instance.cards[index - 1]));
+            List<CardData> sameCard = deck.FindAll(c => c.Equals(card));
             
 
             if (sameCard.Count < 2)
             {
                 deck.Add(GetCard.instance.cards[index - 1]);
                
-                CardData card = GetCard.instance.cards[index - 1];               
+                      
                 Transform temp;                
                 
                 if (sameCard.Count < 1)
                 {
                     temp = Instantiate(DeckObject, contentDeck).transform;
+                    //添加 牌組物件腳本
+                    temp.gameObject.AddComponent<DeckObject>().index = card.index;
                     temp.name = "套牌" + card.name;
                 }
                 else
                 {
                     temp = GameObject.Find("套牌" + card.name).transform;
                 }
-                TextCountDeck.GetComponent<Text>().text = "卡片數量: " + deck.Count + " / 30";
-
+                
                 temp.Find("消耗").GetComponent<Text>().text = card.cost.ToString();
                 temp.Find("卡片名稱").GetComponent<Text>().text = card.name;
                 temp.Find("數量").GetComponent<Text>().text = (sameCard.Count + 1).ToString();
-
+                TextCountDeck.text = "卡片數量: " + deck.Count + " / 30";
             }
            
             
         }
-       
-      
+        if (deck.Count == 30)
+        btnStart.interactable = true;
+
     }
 
     public void DeleteCard(int index)
     {
-        print("刪除牌組卡牌，編號為:" + index);
+        CardData card = GetCard.instance.cards[index - 1];
+
+        List<CardData> sameCard = deck.FindAll(c => c.Equals(card));
+        
+        //刪除卡牌
+        deck.Remove(card);
+
+        Transform temp = GameObject.Find("套牌" + card.name).transform;
+        //相同卡>1
+        //更新牌組物件 數量
+        if (sameCard.Count > 1)
+        {
+            temp.Find("數量").GetComponent<Text>().text = (sameCard.Count - 1).ToString();
+        }
+        //相同卡<1
+        else
+        {
+         //刪除牌組物件
+            Destroy(temp.gameObject);
+        }
+        TextCountDeck.text = "卡片數量: " + deck.Count + " / 30";
+        btnStart.interactable = false;
+      
     }
 }
