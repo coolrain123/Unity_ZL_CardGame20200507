d5cd1e4d8a1da372063503bc3a456d4f1ce15dcc
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-06-23 21:26:36 +0800

    UILINERENSERER

--- Unity_CardGame/Assets/Scripts/AttackCard.cs
+++ Unity_CardGame/Assets/Scripts/AttackCard.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI.Extensions;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(UILineRenderer))]
 public class AttackCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public UILineRenderer line;
+
+    private void Start()
+    {
+        line = GetComponent<UILineRenderer>();
+        line.material = Resources.Load<Material>("線條材質");
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         
